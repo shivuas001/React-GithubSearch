14f30318cd4067bcd7486f5a6935fa1790b2cdae
A SHIVA KUMAR <shivuas605@gmail.com>
2025-09-16 21:47:04 +0530

    second commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 const App = ()=>{
 
 
-  const searchGithub = async (username) => {
-    const { data } = await axios.get('https://api.github.com/users/shivuas001');
+  const [username, setUsername] = useState('shivuas001')
+  
+  const [user, setUser] = useState({});
+
+  const searchGithub = async (name) => {
+    const { data } = await axios.get(`https://api.github.com/users/${name}`);
 
     console.log(data);
+
+    setUser(data);
   }
 
-searchGithub();
+searchGithub(username);
 
   return(
     <>
+    <img src={user.avatar_url} alt="" width="150px"/>
     <h1>Github Search</h1>
+    <h2>{user.name}</h2>
+    <p>{user.location}</p>
+    <p>{user.bio}</p>
     </>
   )
 }
